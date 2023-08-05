8d0170560f5676a6b55a2912ebd55ec432607529
Aniket Das <47720941+dasaniket20002@users.noreply.github.com>
2023-08-06 03:29:52 +0530

    Home Page Added

--- JS/index.js
+++ JS/index.js
@@ -1,4 +1,5 @@
-var navBar = document.getElementById("navBar");
+//NAV BAR Scroll
+let navBar = document.getElementById("navBar");
 
 window.onscroll = function() {scrollFunction()};
 
@@ -9,3 +10,21 @@ function scrollFunction() {
     navBar.classList.remove("navOnScroll");
   }
 }
+
+//HERO Img Scroll
+let scrollImgs = document.querySelectorAll(".scrollImg");
+let scrollNums = document.querySelectorAll(".scrollNum");
+let scrollTxts = document.querySelectorAll(".scrollTxt");
+let i = 0;
+setInterval(() => {
+  scrollImgs[i].classList.remove("active");
+  scrollNums[i].classList.remove("active");
+  scrollTxts[i].classList.remove("active");
+
+  i++;
+  if(i >= scrollImgs.length) i = 0;
+
+  scrollImgs[i].classList.add("active");
+  scrollNums[i].classList.add("active");
+  scrollTxts[i].classList.add("active");
+}, 5000);
