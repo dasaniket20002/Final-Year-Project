70c120d004215413ac97863a768167ba490487e1
Aniket Das <47720941+dasaniket20002@users.noreply.github.com>
2023-08-09 04:27:59 +0530

    Added Sign In and Log In UI

--- JS/index.js
+++ JS/index.js
@@ -2,13 +2,14 @@
 //  NAV BAR BLUR BG 
 
 var navBar = document.querySelector("nav");
-window.onscroll = function() {
-  if (Math.round(window.scrollY) > 0) {
-    navBar.style.backdropFilter = "blur(5px)";
-    navBar.style.boxShadow = "0px 0px 5px 0px rgba(0, 0, 0, 0.25)";
-  } else {
+window.onscroll = function() { defaultOnScroll(); }
+function defaultOnScroll() {
+  if (Math.round(window.scrollY) == 0) {
     navBar.style.backdropFilter = "none";
     navBar.style.boxShadow = "none";
+  } else {
+    navBar.style.backdropFilter = "blur(5px)";
+    navBar.style.boxShadow = "0px 0px 5px 0px rgba(0, 0, 0, 0.25)";
   }
 }
 
@@ -42,29 +43,60 @@ signUpBtns.forEach((btn) => {
       document.body.style.transform = "none";
       navBar.style.transform = "none";
 
+      signUpBtns.forEach((btn2) => {
+        btn2.classList.remove("enableClose");
+      });
+
       // ENABLE SCROLL
-      window.onscroll = function() {}
+      window.onscroll = function() { defaultOnScroll(); }
       document.body.style.overflowY = "auto";
     } else {
-      document.body.style.transform = "translate(-50%, 0%)";
-      navBar.style.transform = "translate(100%, 0%)";
-
       window.scrollTo(window.scrollX, 0);
 
       // DISABLE SCROLL AFTER SCROLL ENDS
       if(Math.round(window.scrollY) == 0) {
         document.body.style.overflowY = "hidden";
+        transformBody();
       } else {
         window.onscroll = function() {
-          console.log(window.scrollY);
+          defaultOnScroll();
           if(Math.round(window.scrollY) == 0) {
             document.body.style.overflowY = "hidden";
+            transformBody();
           }
         }
       }
-
-      
     }
     active = !active;
   });
+});
+
+function transformBody() {
+  document.body.style.transform = "translate(-50%, 0%)";
+  navBar.style.transform = "translate(100%, 0%)";
+
+  signUpBtns.forEach((btn2) => {
+    btn2.classList.add("enableClose");
+  });
+}
+
+// ACCOUNT SECTION MODE SWITCH SIGNUP/LOGIN
+
+let accountContainers = document.querySelectorAll(".accounts");
+let inpBtn_divs = document.querySelectorAll(".inpBtn_div");
+let containerBorder = document.querySelector(".accountsBG");
+
+inpBtn_divs[0].addEventListener("mousedown", function(e) {
+  accountContainers[1].classList.remove("active");
+  accountContainers[0].classList.add("active");
+
+  containerBorder.classList.remove("right");
+  containerBorder.classList.add("left");
+});
+inpBtn_divs[1].addEventListener("mousedown", function(e) {
+  accountContainers[0].classList.remove("active");
+  accountContainers[1].classList.add("active");
+
+  containerBorder.classList.remove("left");
+  containerBorder.classList.add("right");
 });
